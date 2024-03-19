1c79cf76bfa57fe99c0ec55c8c864024b2a173e9
Alex <202300831@post.au.dk>
2024-03-19 13:37:07 +0100

    Cleanup

--- users-microservice — kopia/app/app.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.exceptions.definitions import *
-from app.exceptions.handlers import *
-from app.controllers.user_controller import router as user_router
- 
-app = FastAPI()
-app.add_exception_handler(UserAlreadyExists,user_already_exists_exception_handler)
-app.include_router(user_router)
- 
--- users-microservice — kopia/app/models/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-from bson import ObjectId
-
-class User(BaseModel):   
-    _id: Optional[ObjectId] = None
-    email:str
-    password_hash:str
-
