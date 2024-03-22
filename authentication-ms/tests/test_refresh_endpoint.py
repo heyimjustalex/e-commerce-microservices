c8f7fab6aeea69a421675a766bb4f5687e642447
Alex <202300831@post.au.dk>
2024-03-22 21:43:06 +0100

    Tests: add refresh token tests

--- authentication-ms/tests/test_refresh_endpoint.py
+++ authentication-ms/tests/test_refresh_endpoint.py
@@ -9,9 +9,9 @@ from pymongo.database import Database
 from typing import Any, Callable
 import os
 from app.database.connector import Connector
-from tests.conftest.conftest import client, app, inmemory_database_creation_function
-import datetime
-
+from tests.conftest.conftest import client, inmemory_database_creation_function,app
+from datetime import datetime
+from freezegun import freeze_time
 envs: dict[str, str] = {
     'JWT_ACCESS_TOKEN_SECRET_KEY': 'accesstokenkey',
     'JWT_REFRESH_TOKEN_SECRET_KEY': "refreshtokenkey",
@@ -20,55 +20,26 @@ envs: dict[str, str] = {
     'JWT_REFRESH_TOKEN_EXPIRE_MINUTES': '30'
 }
 
-FAKE_TIME = datetime.datetime(2020, 12, 25, 17, 5, 55)
-
-@pytest.fixture
-def patch_datetime_now(monkeypatch):
-    class mydatetime:
-        @classmethod
-        def now(cls, tz=None):
-            return FAKE_TIME
-    
-    monkeypatch.setattr(datetime, 'datetime', mydatetime)
-    return mydatetime
-  
 
 def test_GivenRefreshToken_When_Refresh_Then_ReturnAccessToken(
     inmemory_database_creation_function: Callable[[], Database[Any]],
     monkeypatch,
-    patch_datetime_now
-
 ) -> None: 
     # Update ENV variables
     monkeypatch.setattr(os, 'environ', envs)
-    monkeypatch.setattr(datetime,'datetime',patch_datetime_now)
-
-     # Given
-    user_data: dict[str, str] = {
-        "email": "aaa@aaa.com",
-        "password": "aaa@aaa.com",
-        "role": "user"
-    }
-
-
-    app.dependency_overrides[Connector.get_db] = inmemory_database_creation_function
-    app.dependency_overrides[datetime.datetime.now] = patch_datetime_now.now
-    # When
-    response: Response = client.post("/api/auth/login", json=user_data)
-    response_json = response.json()
-    print("LOGIN RESPONSE", response_json)
     # Mock DB
-
+    app.dependency_overrides[Connector.get_db] = inmemory_database_creation_function
+          
     # Given aaa@aaa.com refresh token
     user_data: dict[str, str] = {
-        "refresh_token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFhYUBhYWEuY29tIiwicm9sZSI6InVzZXIiLCJleHAiOjE3MTExMzAyMDZ9.FNIg7Fo-EUpL0oZj2f4wfCJaBsOKrWxwHBd7oxY7J9M"
+        "refresh_token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFhYUBhYWEuY29tIiwicm9sZSI6InVzZXIiLCJleHAiOjEzMjY1MDEwMDB9.oVVF9ruy37bocuJar-W0xALIOWgVfvPRdFXL9rCCN78"
     }
-    
+    with freeze_time("2012-01-14"):
     # When
-    response: Response = client.post("/api/auth/refresh", json=user_data)
-    response_json = response.json()
-    print(response_json["access_token"])
+        response: Response = client.post("/api/auth/refresh", json=user_data)
+        response_json = response.json()
 
     # Then
     assert response.status_code == status.HTTP_200_OK
     assert "access_token" in response_json
+    assert response_json["access_token"]=="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFhYUBhYWEuY29tIiwicm9sZSI6InVzZXIiLCJleHAiOjEzMjcxMDQwMDB9.bHk3QXaRko6KJfIGUM4wln11UOTCYmXZ1pDR2Tcszds"
