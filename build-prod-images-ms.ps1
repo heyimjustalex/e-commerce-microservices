84251dd90cb019b2bd647b73f99345849ca3ba55
Alex <202300831@post.au.dk>
2024-04-20 16:30:27 +0200

    Infrastracture: Kubernetes authentication working

--- build-prod-images-ms.ps1
+++ build-prod-images-ms.ps1
@@ -2,24 +2,24 @@
 $buildCommand1 = "docker build -t heyimjustalex/authentication-ms -f ./backend/authentication-ms/Dockerfile.prod ./backend/authentication-ms/"
 $buildCommand2 = "docker build -t heyimjustalex/orders-ms -f ./backend/orders-ms/Dockerfile.prod ./backend/orders-ms/"
 $buildCommand3 = "docker build -t heyimjustalex/products-ms -f ./backend/orders-ms/Dockerfile.prod ./backend/products-ms/"
-$buildCommand4 = "docker build -t heyimjustalex/authentication-db ./db/authentication-db/"
-$buildCommand5 = "docker build -t heyimjustalex/orders-db ./db/orders-db/"
-$buildCommand6 = "docker build -t heyimjustalex/products-db ./db/products-db/"
-$buildCommand7 = "docker build -t heyimjustalex/gateway-ms ./backend/gateway-ms/"
+$buildCommand4 = "docker build -t heyimjustalex/gateway-ms ./backend/gateway-ms/"
+
+$buildCommand5 = "docker build -t heyimjustalex/authentication-db ./db/authentication-db/"
+$buildCommand6 = "docker build -t heyimjustalex/orders-db ./db/orders-db/"
+$buildCommand7 = "docker build -t heyimjustalex/products-db ./db/products-db/"
+
 $buildCommand8 = "docker build -t heyimjustalex/frontend -f ./frontend/app/Dockerfile.prod ./frontend/app"
 $buildCommand9 = "docker build -t heyimjustalex/nginx-proxy/prod ./nginx-proxy/prod"
-
-
 
 $pushCommand1 = "docker push heyimjustalex/authentication-ms"
 $pushCommand2 = "docker push heyimjustalex/orders-ms"
 $pushCommand3 = "docker push heyimjustalex/products-ms"
+$pushCommand4 = "docker push heyimjustalex/gateway-ms"
 
-$pushCommand4 = "docker push heyimjustalex/authentication-db"
-$pushCommand5 = "docker push heyimjustalex/orders-db"
-$pushCommand6 = "docker push heyimjustalex/products-db"
+$pushCommand5 = "docker push heyimjustalex/authentication-db"
+$pushCommand6 = "docker push heyimjustalex/orders-db"
+$pushCommand7 = "docker push heyimjustalex/products-db"
 
-$pushCommand7 = "docker push heyimjustalex/gateway-ms"
 $pushCommand8 = "docker push heyimjustalex/frontend"
 $pushCommand9 = "docker push heyimjustalex/nginx-proxy"
 
