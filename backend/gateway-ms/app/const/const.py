ccd5104741fda07ab6f011a880ceab5e0f358315
Alex <202300831@post.au.dk>
2024-04-20 20:53:01 +0200

    Infrastracture: Add working broker and zookeeper

--- backend/gateway-ms/app/const/const.py
+++ backend/gateway-ms/app/const/const.py
@@ -1,16 +1,19 @@
-
+import os
 # Define mappings from gateway endpoints to backend services
+authentication_service_url:str = os.getenv("AUTHENTICATION_SERVICE_URL", "http://authentication-ms:8000") 
+products_service_url:str = os.getenv("PRODUCTS_SERVICE_URL", "http://products-ms:8000") 
+orders_service_url:str = os.getenv("ORDERS_SERVICE_URL", "http://orders-ms:8000") 
+
 endpoint_redirect_map: dict[str, str] = {
-        "/api/products": "http://products-ms:8000",  
-        "/api/categories": "http://products-ms:8000",  
-        "/api/register": "http://authentication-ms:8000",
-        # "/api/login": "http://authentication-ms:8000",
-        "/api/login": "http://authentication-ms-svc.default:8000",
-        "/api/refresh": "http://authentication-ms:8000",
-        "/api/orders":"http://orders-ms:8000",
-        "/api/products_error":"http://products-ms:8000",  
-        "/api/authentication_error":"http://authentication-ms:8000",  
-        "/api/orders_error":"http://orders-ms:8000",  
+        "/api/products": products_service_url,  
+        "/api/categories": products_service_url, 
+        "/api/register": authentication_service_url,
+        "/api/login": authentication_service_url,
+        "/api/refresh": authentication_service_url,
+        "/api/orders":orders_service_url,
+        "/api/products_error":products_service_url,  
+        "/api/authentication_error":authentication_service_url,  
+        "/api/orders_error":orders_service_url,  
     }
 
 endpoint_access_map: dict[tuple[str,str,str],bool] = {
