d474f4f21aa47f2f3a1e7b83e2f949f5af640073
Alex <202300831@post.au.dk>
2024-04-29 09:40:39 +0200

    Frontend: Update front page

--- frontend/app/src/components/Home/Home.jsx
+++ frontend/app/src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="container">
       <h1 className="display-4 text-center mt-5">
-        !@# !! Welcome to microservice e-commerce app !! !@#
+        ! Welcome to microservice e-commerce app !
       </h1>
     </div>
   );
